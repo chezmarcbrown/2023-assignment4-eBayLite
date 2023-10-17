b0791675847ac37f4d9127c7cdad6368307417ce
Jay-Mark Pascua <jpascua3@alaska.edu>
2023-10-16 17:17:08 -0800

    Added some template files

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing():
+    title = models.CharField(('title'), max_length=150, blank=False)
+    description = models.TextField(('description'), max_length=300, blank=False)
+    bid = models.DecimalField(('bid'), decimal_places=2, blank=False)
+    is_active = models.BooleanField(('is active'), default=True, blank=False)
+    pass
+
+class Bid():
+    pass
+
+class Comment():
+    pass
