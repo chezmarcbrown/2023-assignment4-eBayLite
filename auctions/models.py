4075e718573e80b31eb6fe079a58b087dd4e35e5
Luke Pratt <lukepratt@Lukes-MacBook-Air.local>
2023-10-26 17:11:43 -0800

    iteration 1

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,49 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    wishlist = models.ManyToManyField('Auction', related_name='wishlist', blank=True)
+
+class Auction(models.Model):
+
+    FASHION = "Fashion"
+    TOYS = "Toys"
+    ELECTRONICS = "Electronics"
+    HOME = "Home"
+
+    CATEGORIES = [
+        (FASHION, "Fashion"),
+        (TOYS, "Toys"),
+        (ELECTRONICS, "Electronics"),
+        (HOME, "Home")
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    #starting_bid = models.DecimalField(max_digits=12, decimal_places=2, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORIES, blank=False)
+    image_url = models.URLField(default='')
+    #bid_counter = models.IntegerField(default=1)
+    #created_at = models.DateTimeField(auto_now_add=True)
+    #active = models.BooleanField(default=True)
+    #winner = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title}: made by {self.user.username}'
+    
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)# the user making the bid
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)# the auction they are bidding on
+    bid =  models.DecimalField(decimal_places=2, max_digits=8)# the amount they are bidding for
+
+    def __str__(self):
+        return f'{self.user.username} bidded {self.bid} on {self.auction.title}'# /admin displays this
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username} commented: {self.comment} on {self.auction.title}'
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
-from .models import User
+from .models import Auction, User, Bid, Comment
+from .forms import AuctionForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "auctions":Auction.objects.all()
+    })
 
 
 def login_view(request):
@@ -22,7 +25,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("auctions:index"))
         else:
             return render(request, "auctions/login.html", {
                 "message": "Invalid username and/or password."
@@ -33,7 +36,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("auctions:index"))
 
 
 def register(request):
@@ -58,6 +61,99 @@ def register(request):
                 "message": "Username already taken."
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("auctions:index"))
     else:
         return render(request, "auctions/register.html")
+
+def create_listing(request):
+    if request.method == "POST":
+        f = AuctionForm(request.POST)
+        if f.is_valid():
+            auction = Auction(user=request.user, **f.cleaned_data)
+            auction.save()
+            price  = auction.price
+            bid = Bid(user=request.user, auction=auction, bid=price)
+            bid.save()
+            return HttpResponseRedirect(reverse('auctions:index'))
+        else:
+            return render(request, 'auctions/create_listing.html', {
+                'form': f
+            })
+    return render(request, "auctions/create_listing.html", {
+        "form": AuctionForm()
+    })
+
+def auction(request, auction_id):# show the auction you click on
+    auction = get_object_or_404(Auction, id=auction_id)
+    bid = get_object_or_404(Bid, auction=auction)
+    comments = Comment.objects.filter(auction=auction)
+    return render(request, "auctions/auction.html", {
+        'auction': auction,
+        "bid": bid,
+        "user": request.user,
+        "comments": comments,
+        "commentform": CommentForm
+    })
+
+def categories(request):#show a list of all categories and be able to select on and go to category
+    categories = Auction.CATEGORIES
+    categories_fixed = []
+    for x in categories:
+        categories_fixed.append(x[1])
+    return render(request, "auctions/categories.html", {
+        'categories': categories_fixed
+    })
+
+def category(request, cat_name):# show a list of all auctions within the category you selected
+    auctions = Auction.objects.filter(category=cat_name)
+    return render(request, "auctions/category.html", {
+        'auctions': auctions,
+        "name": cat_name
+    })
+
+@login_required(login_url='auctions/login.html')
+def wishlist(request):
+    return render(request, "auctions/wishlist.html", {
+        'wishlist': request.user.wishlist.all()# displays all on wishlist
+    })
+
+@login_required(login_url='auctions/login.html')
+def addwishlist(request, auction_id):
+    auction = get_object_or_404(Auction, id=auction_id)
+    request.user.wishlist.add(auction)# adds to the wishlist stored in User
+    request.user.save()# saves to database
+    return HttpResponseRedirect(reverse("auctions:wishlist"))
+
+@login_required(login_url='auctions/login.html')
+def removewishlist(request, auction_id):
+    auction = get_object_or_404(Auction, id=auction_id)
+    request.user.wishlist.remove(auction)
+    request.user.save()
+    return HttpResponseRedirect(reverse("auctions:wishlist"))
+
+@login_required(login_url='auctions/login.html')
+def makebid(request, bid_id, auction_id):
+    auction = get_object_or_404(Auction, id=auction_id)
+    price = request.POST['bid']# gets input from form in auction.html
+    if price != None:
+        price = float(price)
+        if price > get_object_or_404(Bid, id=bid_id).bid:
+            bid = get_object_or_404(Bid, id=bid_id)
+            bid.user = request.user
+            bid.bid = price
+            #bid.auction.price = price
+            bid.save()
+            auction.price = price
+            auction.save()
+            return HttpResponseRedirect(reverse('auctions:index'))
+        
+@login_required(login_url='auctions/login.html')
+def comment(request, auction_id):
+    auction = get_object_or_404(Auction, id=auction_id)
+    if request.method == "POST":
+        f = CommentForm(request.POST)
+        if f.is_valid():
+            text = f.cleaned_data
+            comment = Comment(user=request.user,auction=auction, **text)
+            comment.save()
+            return HttpResponseRedirect(reverse('auctions:index'))
