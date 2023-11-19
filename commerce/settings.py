68f2f0617b4d34a27ac675d59817c11f2a3a7cac
David Rodriguez <rodriguez.davidp@outlook.com>
2023-11-19 11:39:21 -0900

    working on crispy forms and bootstrap4

--- commerce/settings.py
+++ commerce/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    # 'bootstrap5',
+    'bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
