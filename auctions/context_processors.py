409e670c8ee39ffeba7f2edddde007ceea1dacc2
David Rodriguez <rodriguez.davidp@outlook.com>
2023-11-19 12:36:33 -0900

    Add category information and improve image presentation
    A new function 'category_info' was added in 'context_processors.py' to provide info about whether the current request path starts with '/category/'. This data is then used to manipulate the template 'layout.html'.
    
    In the 'uaa490styles.css' file, a new class '.detail-img' was added to handle the presentation of the images in a more detailed view, which is used in 'listing.html'.
    
    Setting for 'category_info' was added in 'settings.py'.
    
    The buttons in 'create_listing.html' were slightly adjusted for uniformity and redundant links removed.
    
    All the changes aim to improve user navigation and user interface.

--- auctions/context_processors.py
+++ auctions/context_processors.py
@@ -3,3 +3,8 @@ from .models import Category
 
 def categories(request):
     return {'categories': Category.objects.all()}
+
+
+def category_info(request):
+    is_categories = request.path.startswith('/category/')
+    return {'is_categories': is_categories}
--- commerce/settings.py
+++ commerce/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'auctions.context_processors.categories',
+                'auctions.context_processors.category_info',
             ],
         },
     },
